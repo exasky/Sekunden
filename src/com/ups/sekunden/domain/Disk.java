25f7761d5632adc3e08de78cf091359bc01aab26
Simar Jeremy <exasky@live.fr>
2014-02-07 09:10:46 +0100

    Domain class

---
+++ src/com/ups/sekunden/domain/Disk.java
@@ -0,0 +1,41 @@
+package com.ups.sekunden.domain;
+
+public class Disk {
+	private int xCenter;
+	private int yCenter;
+	private int radius;
+	private int msTime;
+	
+	public int getxCenter() {
+		return xCenter;
+	}
+	
+	public void setxCenter(int xCenter) {
+		this.xCenter = xCenter;
+	}
+	
+	public int getyCenter() {
+		return yCenter;
+	}
+	
+	public void setyCenter(int yCenter) {
+		this.yCenter = yCenter;
+	}
+	
+	public int getRadius() {
+		return radius;
+	}
+	
+	public void setRadius(int radius) {
+		this.radius = radius;
+	}
+	
+	public int getMsTime() {
+		return msTime;
+	}
+	
+	public void setMsTime(int msTime) {
+		this.msTime = msTime;
+	}
+	
+}
