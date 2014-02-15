4d97750e1fccf33501ff8045c4e7e575635aca24
jbarreau <julien.barreau@laposte.net>
2014-02-15 17:29:50 +0100

    adding sound

--- src/com/ups/sekunden/GameActivity.java
+++ src/com/ups/sekunden/GameActivity.java
@@ -1,6 +1,7 @@
 package com.ups.sekunden;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.*;
@@ -13,15 +14,18 @@ import com.ups.sekunden.rythm.RythmRandomImpl;
 /**
  * Created by julien on 07/02/14.
  */
-
 public class GameActivity extends Activity implements IRythmListener {
 	public String classTag = "GameActivity";
+    private static int SOUND_ID_HILIGHT_TRIBE_DRAGONFLY  =  R.raw.hilighttribedragonfly;
+
 
 	private DiskImageView diskImage;
 	private IRythm rythm;
 	private Thread rythmTh;
 	private Integer xShift = null;
 	private Integer yShift = null;
+    private MediaPlayer backgroundMusic;
+
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -33,6 +37,12 @@ public class GameActivity extends Activity implements IRythmListener {
 
 		setContentView(R.layout.game_layout);
 		Display defaultDisplay = getWindowManager().getDefaultDisplay();
+
+        //play musique
+        backgroundMusic = MediaPlayer.create(this, SOUND_ID_HILIGHT_TRIBE_DRAGONFLY);
+        backgroundMusic.setVolume(1, 1);
+        backgroundMusic.setLooping(true);
+        backgroundMusic.start(); // no need to call prepare(); create() does that for you
 
         diskImage = (DiskImageView) findViewById(R.id.viewDisk);
 		rythm = new RythmRandomImpl(defaultDisplay.getHeight(),
@@ -47,6 +57,7 @@ public class GameActivity extends Activity implements IRythmListener {
 	protected void onDestroy() {
 		super.onDestroy();
 		rythm.onStop();
+        backgroundMusic.stop();
 	}
 
 	@Override
@@ -75,15 +86,35 @@ public class GameActivity extends Activity implements IRythmListener {
     public void doPause(View view){
         if (rythm.isPaused()) {
         	try {
+        	    //---gradient sound resume
+                backgroundMusic.setVolume(0.333f, 0.333f); // setVolume (left, rigt) float values between 0 and 1
+                backgroundMusic.start();
 				Thread.sleep(400);
-			} catch (InterruptedException e) {
-			}
-            rythm.onResumed();
-            diskImage.resume();
+
+                backgroundMusic.setVolume(0.666f, 0.666f);
+                Thread.sleep(400);
+
+                backgroundMusic.setVolume(1.0f, 1.0f);
+                //---END OF gradient sound reume
+                rythm.onResumed();
+                diskImage.resume();
+
+			} catch (InterruptedException e) {} //caued thread.sleep
         }
         else {
             rythm.onPaused();
             diskImage.pause();
+
+            try {
+                //---START OF gradient sound pause
+                backgroundMusic.setVolume(0.666f, 0.666f);
+                Thread.sleep(400);
+                backgroundMusic.setVolume(0.333f, 0.333f);
+                Thread.sleep(400);
+                backgroundMusic.pause();
+                //---END OF gradient sound pause
+
+            } catch (InterruptedException e) { } //caused thread.sleep
         }
     }
 }
